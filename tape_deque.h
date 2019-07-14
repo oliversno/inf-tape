eced822eacba608d394a60d6c9b87b5a80fa9d39
Sean Oliver <snoliver@umich.edu>
2019-07-14 14:35:13 -0400

    added tape_deque.h

---
+++ tape_deque.h
@@ -0,0 +1,25 @@
+#ifndef TAPE_DEQUE_H
+#define TAPE_DEQUE_H
+
+#include <deque>
+
+template <typename T>
+class tapeDeque{
+    public:
+        tapeDeque(){
+            deque.resize(1);
+            it = deque.begin();
+        }
+
+        T get() const;
+        void set(const T in);
+        void increment() const;
+        void increment(const size_t delta) const;
+        void decrement() const;
+        void decrement(const size_t delta) const;
+    private:
+        std::deque<T> deque;
+        std::deque<T>::iterator it;
+};
+
+#endif
