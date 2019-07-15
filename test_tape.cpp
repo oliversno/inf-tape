b238718fc26a5b14f412b7ddf815506b86e3e2e4
Sean Oliver <snoliver@umich.edu>
2019-07-15 13:41:41 -0400

    added more test cases

--- test_tape.cpp
+++ test_tape.cpp
@@ -1,2 +1,68 @@
 #include "inf_tape.cpp"
 #include "tape_deque.h"
+#include <gtest/gtest.h>
+
+TEST(ApplyTest, InitInf){
+    infTape<int> t;
+    EXPECT_EQ(t.get(), 0);
+}
+
+TEST(ApplyTest, InitDeque){
+    tapeDeque<int> t;
+    EXPECT_EQ(t.get(), 0);
+}
+
+TEST(ApplyTest, IncInf){
+    infTape<int> t;
+    t.increment();
+    EXPECT_EQ(t.get(), 0);
+}
+
+TEST(ApplyTest, IncDeque){
+    tapeDeque<int> t;
+    t.increment();
+    EXPECT_EQ(t.get(), 0);
+}
+
+TEST(ApplyTest, DecInf){
+    infTape<int> t;
+    t.decrement();
+    EXPECT_EQ(t.get(), 0);
+}
+
+TEST(ApplyTest, DecDeque){
+    tapeDeque<int> t;
+    t.decrement();
+    EXPECT_EQ(t.get(), 0);
+}
+
+TEST(ApplyTest, SetInf){
+    infTape<int> t;
+    t.set(1);
+    EXPECT_EQ(t.get(), 1);
+}
+
+TEST(ApplyTest, SetDeque){
+    tapeDeque<int> t;
+    t.set(1);
+    EXPECT_EQ(t.get(), 1);
+}
+
+TEST(ApplyTest, MoveInf){
+    infTape<int> t;
+    t.set(1);
+    t.increment(2);
+    t.decrement();
+    t.decrement();
+    EXPECT_EQ(t.get(), 1);
+}
+
+TEST(ApplyTest, MoveDeque){
+    tapeDeque<int> t;
+    t.set(1);
+    t.increment(2);
+    t.decrement();
+    t.decrement();
+    EXPECT_EQ(t.get(), 1);
+}
+
